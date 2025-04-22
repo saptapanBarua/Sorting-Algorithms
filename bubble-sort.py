2163ad0daac415c838fab5158692a6a1c823c91a
saptapanBarua <sapta.barua@gmail.com>
2025-04-22 00:29:18 -0700

    Bubble Sort error fixed

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,12 +1,12 @@
 '''Bubble Sort'''
 
 def bubble_sort(numbers):
-	for i in range(len(nums)):
-		for j in range(len(nums)-1-i):
-			if nums[j] > nums[j+1]:
-				tmp = nums[j]
-				nums[j] = nums[j+1]
-				nums[j+1] = tmp
+	for i in range(len(numbers)):
+		for j in range(len(numbers)-1-i):
+			if numbers[j] > numbers[j+1]:
+				tmp = numbers[j]
+				numbers[j] = numbers[j+1]
+				numbers[j+1] = tmp
 
 if __name__=='__main__': 
 	nums = [34, 89, 21, 13, 90, 71, 37, 67, 45, 17]
--- selection-sort.py
+++ selection-sort.py
@@ -2,6 +2,7 @@ nums = [48, 59, 12, 71, 37]
 print(nums)
 
 for i in range(len(nums)):
+	min = nums[i]
 	for j in range(i, len(nums)):
 			pass
 
