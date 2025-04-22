7de14b7da31158e9a1dafb78a37836f7fa91356f
saptapanBarua <sapta.barua@gmail.com>
2025-04-22 00:12:18 -0700

    Bubble sort done

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,13 +1,14 @@
 '''Bubble Sort'''
 
+def bubble_sort(numbers):
+	for i in range(len(nums)):
+		for j in range(len(nums)-1-i):
+			if nums[j] > nums[j+1]:
+				tmp = nums[j]
+				nums[j] = nums[j+1]
+				nums[j+1] = tmp
+
 nums = [34, 89, 21, 13, 90, 71, 37, 67, 45, 17]
 print(nums)
-
-for i in range(len(nums)):
-	for j in range(len(nums)-1-i):
-		if nums[j] > nums[j+1]:
-			tmp = nums[j]
-			nums[j] = nums[j+1]
-			nums[j+1] = tmp
-			
+bubble_sort(nums)			
 print(nums)
---
+++ selection-sort.py
@@ -0,0 +1,8 @@
+nums = [48, 59, 12, 71, 37]
+print(nums)
+
+for i in range(len(nums)):
+	for j in range(i, len(nums)):
+			pass
+
+print(nums)
